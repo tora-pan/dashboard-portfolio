3bf3c86f6b8d259d3265bf19c1269d73abc9cc01
tora_pan <travis.pandos@gmail.com>
2022-03-12 23:33:53 -0800

    initial commit

---
+++ src/sections/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus velit
+      molestias temporibus incidunt animi ea voluptates debitis, harum aliquam
+      rerum! Alias vero obcaecati, quidem eveniet vitae illo, autem quisquam
+      iste esse nisi possimus itaque quas sit mollitia exercitationem animi!
+      Iusto, autem? Nisi magni eius quibusdam nulla temporibus neque, esse non?
+      Obcaecati dicta exercitationem perspiciatis et commodi incidunt velit
+      labore minus, officia eum doloribus dolorem corrupti delectus, alias nihil
+      autem, placeat quia inventore voluptate dignissimos in dolor? Rerum amet
+      impedit expedita officia tempore. Ullam aspernatur possimus corrupti
+      ducimus autem perferendis, iure nemo? Sed culpa nostrum maxime ea officiis
+      voluptates vero aperiam.
+    </div>
+  );
+};
+
+export default About;
